e5f4c1191b6a75615e1e72742c5af82875ac7ce3
Bahubali P Fuladi <bpfuladi@gmail.com>
2017-07-04 09:54:02 +0530

    Update shifter.ino

--- shifter.ino
+++ shifter.ino
@@ -1,49 +1,46 @@
 
 #include"value.h"
-
-int latchPin = 4;    //connected to pin 12 (ST_CP) of both shift register ic
-
-int clockPin = 5;    //connected to pin 11 (SH_CP) of both shift register ic
-
-int dataPin = 6;    //connected to pin 12 (DS) of first shift register ic
-
+int latchPin = 4; //connected to pin 12 (ST_CP) of both shift register ic
+int clockPin = 5; //connected to pin 11 (SH_CP) of both shift register ic
+int dataPin = 6; //connected to pin 12 (DS) of first shift register ic
 void setup()
 {
-  pinMode(latchPin, OUTPUT);  //seting direction of pins
+  pinMode(latchPin, OUTPUT); //seting direction of pins
   pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);  
+  pinMode(dataPin, OUTPUT);
   Serial.begin(9600);
   //convertion relay status in array to bytes
   byte firstByte = 0;
   byte secondByte = 0;
-  for(int i=0;i<8;i++)
+  Serial.println("B1***************************");
+  for (int i = 0; i < 8; i++)
   {
-    firstByte = firstByte << i;
-    firstByte = firstByte | (relay[i]&0x01);
+    firstByte = firstByte << 1;
+    firstByte = firstByte | (relay[i] & 0x01);
+    Serial.print(relay[i]);
+    Serial.print(" ----  ");
+    Serial.println(firstByte);
   }
-  
-  for(int i=0;i<8;i++)
+  Serial.println("B1***************************");
+
+  for (int i = 0; i < 8; i++)
   {
-    secondByte = secondByte << i;
-    secondByte = secondByte | (relay[i+8]&0x01);
+    secondByte = secondByte << 1;
+    secondByte = secondByte | (relay[i + 8] & 0x01);
+    Serial.print(relay[i]);
+    Serial.print(" ----  ");
+    Serial.println(secondByte);
   }
   Serial.print("firstByte");
   Serial.println(firstByte);
-  
   Serial.print("secondByte");
   Serial.println(secondByte);
-  
-  digitalWrite(latchPin, LOW);  //make the latch pin low. so that output will not change during shifting
-  
-  shiftOut(dataPin, clockPin, MSBFIRST, firstByte);   //shift out first byte(relay#1 to #8)
-  shiftOut(dataPin, clockPin, MSBFIRST, secondByte);  //shift out second byte(relay#9 to #16)
-  
-  digitalWrite(latchPin, HIGH);  //make the latch pin high. so thate relay status will be writen on it
-  
+  digitalWrite(latchPin, LOW); //make the latch pin low. so that output will not change during shifting
+  shiftOut(dataPin, clockPin, MSBFIRST, firstByte); //shift out first byte(relay#1 to #8)
+  shiftOut(dataPin, clockPin, MSBFIRST, secondByte); //shift out second byte(relay#9 to #16)
+  digitalWrite(latchPin, HIGH); //make the latch pin high. so thate relay status will be writen on it
 }
-
 void loop()
 {
-  //do noting 
+  //do noting
 }
-
